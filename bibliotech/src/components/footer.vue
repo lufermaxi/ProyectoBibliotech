26910b0cff99ae889cb84c77df5cd89623642001
muakarada <keymoca@hotmail.com>
2021-10-01 20:51:48 -0500

    Update design in bootstrap

--- bibliotech/src/components/footer.vue
+++ bibliotech/src/components/footer.vue
@@ -1,47 +1,48 @@
 <template>
   <div>
-    <footer class="footer-section">
-      <!--Inicio del contenido del footer-->
-      <div class="blue-1">
+    <b-container fluid>
+      <b-row class="footer-row">
         <!--Clase para diseño de colores: azul-->
-        <div class="logo-foot">
-          <img src="../assets/images/white-logo.png" alt="Logo del footer" />
-        </div>
-        <!--Info de creación-->
-        <div class="info-foot">
-          <h2>Contáctanos:</h2>
-          <h3>© Bibliotech 2021</h3>
-          <h3>-Todos los derechos reservados-</h3>
-          <h3>Cookies & policy privacy</h3>
-        </div>
-        <!--Lista de íconos-->
-        <div class="icons-foot">
-          <ul>
-            <li>
-              <a href="https://github.com/afar-cmyk/ProyectoBibliotech"
-                >Github</a
-              >
-            </li>
-            <li>
-              <a href="https://web.telegram.org/z/#-540770927">Telegram</a>
-            </li>
-          </ul>
-        </div>
-        <!--Lista de navegación de footer-->
-        <div class="nav-foot">
-          <ul>
-            <li><router-link to="/">Inicio</router-link></li>
-            <li><router-link to="Trade">Préstamo</router-link></li>
-            <li><router-link to="About">Acerca de</router-link></li>
-            <li><router-link to="Team">Equipo</router-link></li>
-            <li><router-link to="Login">Log In</router-link></li>
-          </ul>
-        </div>
-      </div>
-      <!--Clases para diseño de colores de fondo de footer: naranjas-->
-      <div class="orange-2"></div>
-      <div class="orange-3"></div>
-      <div class="orange-4"></div>
-    </footer>
+        <b-col cols="12"  class="blue-1" >
+          <!--Columna de diseño-->
+          <b-col class="logo-foot" sm="12" lg="3">
+            <img src="../assets/images/white-logo.png" alt="Logo del footer" />
+          </b-col>
+          <b-col class="info-foot" sm="12" lg="3">
+            <!--Info de creación-->
+            <h2>Contáctanos:</h2>
+            <h3>© Bibliotech 2021</h3>
+            <h3>--Todos los derechos reservados--</h3>
+            <h3>Cookies & privacy policy</h3>
+          </b-col>
+          <b-col class="icons-foot" sm="12" lg="3">
+            <!--Lista de íconos-->
+            <a href="https://github.com/afar-cmyk/ProyectoBibliotech">
+              <h6>Bibliotech</h6>
+              <b-icon class="git-icon" icon="github"></b-icon>
+            </a>
+            <a href="https://web.telegram.org/z/#-540770927">
+              <h6>Grupo 1/21</h6>
+              <b-icon class="tel-icon" icon="telephone"></b-icon>
+            </a>
+          </b-col>
+          <b-col class="nav-foot" sm="12" lg="3">
+            <!--Lista de navegación de footer-->
+            <ul>
+              <li><router-link to="/">Inicio</router-link></li>
+              <li><router-link to="Trade">Préstamo</router-link></li>
+              <li><router-link to="About">Acerca de</router-link></li>
+              <li><router-link to="Team">Equipo</router-link></li>
+              <li><router-link to="Login">Log In</router-link></li>
+            </ul>
+          </b-col>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col class="orange-2" cols="4"><br /></b-col>
+        <b-col class="orange-3" cols="4"><br /></b-col>
+        <b-col class="orange-4" cols="4"><br /></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
--- bibliotech/src/views/Home.vue
+++ bibliotech/src/views/Home.vue
@@ -3,15 +3,20 @@
     <NavBar />
     <main class="main-section">
       <!--Inicio de contenido de la página index-->
-      <div class="line-container">
-        <h1>¡Bienvenido!</h1>
-        <h3>a nuestro sistema bibliotecario</h3>
-      </div>
-      <!--Sección para imagen central de los libros de Bibliotech-->
-      <section class="wrapper-main">
-        <div class="banner-image">
-          <img src="../assets/images/index-image.png" alt="Banner de inicio" />
-        </div>
+      <b-row class="line-container">
+        <b-col>
+          <h1>¡Bienvenido!</h1>
+          <h3>a nuestro sistema bibliotecario</h3>
+        </b-col>
+      </b-row>
+      |
+      <b-container class="wrapper-main" fluid>
+        <!--Sección para imagen central de los libros de Bibliotech-->
+        <b-row>
+          <b-col cols="12" md="6" class="banner-image">
+            <img src="../assets/images/index-image.png" alt="Banner de inicio"/>
+          </b-col>
+        </b-row>
         <!--Saludo e invitación a ir a préstamo-->
         <div class="banner-text">
           <h3>¡Hola!</h3>
@@ -20,15 +25,14 @@
             <router-link to="Trade">Préstamo</router-link>
           </div>
         </div>
-      </section>
-      <!---Fin del contenido de la página index-->
+      </b-container>
     </main>
     <Footer />
   </div>
 </template>
 <script>
-import NavBar from '../components/navbar.vue'
-import Footer from '../components/footer.vue'
+import NavBar from "../components/navbar.vue";
+import Footer from "../components/footer.vue";
 
 export default {
   components: {
